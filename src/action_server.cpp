5e86946eb69cccf501f0f167e3d8ba519de3ee48
mujuburrehman.gouri <mujuburrehman.gouri@hansrobot.de>
2021-01-21 10:59:12 +0100

    Cartesian Velocity and Energy

--- src/action_server.cpp
+++ src/action_server.cpp
@@ -1,11 +1,15 @@
 ﻿#include "ros/ros.h"
 #include <iostream>
-#include "actionlib/server/simple_action_server.h"
-#include "ros_action_server/MyMsgAction.h"
 #include "math.h"
 #include <array>
 #include <Eigen/Eigen>
 #include <chrono>
+
+/* ros stuff */
+#include "actionlib/server/simple_action_server.h"
+#include "ros_action_server/MyMsgAction.h"
+#include <dynamic_reconfigure/server.h>
+#include <ros_action_server/PidConfig.h>
 
 /* ros msgs */
 #include "std_msgs/Empty.h"
@@ -25,6 +29,11 @@
 #include <kdl/jntspaceinertiamatrix.hpp>
 #include <kdl/rotational_interpolation_sa.hpp>
 #include <kdl/frames.hpp>
+#include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/jacobian.hpp>
+
+#include <kdl/chainidsolver_recursive_newton_euler.hpp> // ?
+
 
 using namespace std;
 using namespace std::chrono;
@@ -36,6 +45,9 @@ class MoveRobotAction{
 protected:
   ros::NodeHandle nh;
   actionlib::SimpleActionServer<ros_action_server::MyMsgAction> action_server;
+  dynamic_reconfigure::Server<ros_action_server::PidConfig> server;
+  dynamic_reconfigure::Server<ros_action_server::PidConfig>::CallbackType f;
+
   std::string action_name;                    // name of my action server
   ros_action_server::MyMsgFeedback feedback; // variables stores the feedback
   ros_action_server::MyMsgResult result;    // variables stores the result
@@ -60,6 +72,7 @@ protected:
   Eigen::MatrixXf q_cur, qdot_cur;
   std::array<float, N_JOINT> k_p;
   std::array<float, N_JOINT> k_d;
+  double total_E;
 
 public:
   //constructor
@@ -67,10 +80,12 @@ public:
     action_server(nh, name, boost::bind(&MoveRobotAction::actionCb, this, _1),false),
     action_name(name)
   {
-   initializeVariables();
-   initializeSubscribers();
-   initializePublishers();
-   action_server.start();
+    f = boost::bind(&MoveRobotAction::reconfigureCb, this, _1, _2);
+    server.setCallback(f);
+    initializeVariables();
+    initializeSubscribers();
+    initializePublishers();
+    action_server.start();
   }
 
   ~MoveRobotAction(void){} // delete
@@ -87,7 +102,7 @@ private:
     "tau_5",
     "tau_6",
   };
-  std::string dataTopicNames[12] = {
+  std::string dataTopicNames[18] = {
     "pose_error_1",
     "pose_error_2",
     "pose_error_3",
@@ -100,17 +115,25 @@ private:
     "velo_error_4",
     "velo_error_5",
     "velo_error_6",
+    "CartVel_1",
+    "CartVel_2",
+    "CartVel_3",
+    "CartVel_4",
+    "CartVel_5",
+    "CartVel_6",
   };
 
   std::array<int, N_JOINT> map_joint_states;
+  KDL::Wrenches f_ext; // ?
 
   void initializeVariables(void)
   {
     jointPosCurrent.resize(N_JOINT), jointVelCurrent.resize(N_JOINT), jointEffort.resize(N_JOINT);
     q_cur.resize(N_JOINT,1); qdot_cur.resize(N_JOINT,1);
-    map_joint_states={2, 1, 0, 3, 4, 5};      // to read joint_states in order
-    k_p={26, 85, 80,  12,  12,  12};          // specify p and d gains
-    k_d={10, 12, 12,  6,  6,  6};
+    map_joint_states={2, 1, 0, 3, 4, 5};        // to read joint_states in order
+    k_p={26, 85, 80,  860,  650,  12};          // specify p and d gains
+    k_d={10, 12, 12,  200,  65,  6};
+    total_E = 0.0;
     // BEGIN
     float mass = 5 ; double Ixx, Iyy, Izz; double l= 0.08, r = l/2.0; // from URDF
 
@@ -151,6 +174,7 @@ private:
     {
       ROS_ERROR("[AS] ERROR in size of joint variables ! ");
     }
+    f_ext.resize(ns); // ?
   }
 
   void initializeSubscribers(void)
@@ -167,12 +191,24 @@ private:
       pose_pub = nh.advertise<std_msgs::Float64>(tauTopicNames[j], 1);
       pose_multiple_pub.push_back(pose_pub);
     }
-    for (short int j = 0; j < 12; j++)
+    for (short int j = 0; j < 18; j++)
     {
       data_pub = nh.advertise<std_msgs::Float64>(dataTopicNames[j], 1);
       data_multiple_pub.push_back(data_pub);
     }
     ROS_INFO("[AS] Publisher Initialized");
+  }
+
+  void reconfigureCb(ros_action_server::PidConfig &config, uint32_t level){
+
+    k_p[0]=config.p0; k_d[0]=config.d0;
+    k_p[1]=config.p1; k_d[1]=config.d1;
+    k_p[2]=config.p2; k_d[2]=config.d2;
+    k_p[3]=config.p3; k_d[3]=config.d3;
+    k_p[4]=config.p4; k_d[4]=config.d4;
+    k_p[5]=config.p5; k_d[5]=config.d5;
+
+    ROS_INFO("Reconfigure Requested");
   }
 
   void subscriberCb(const sensor_msgs::JointStateConstPtr &msg)
@@ -192,6 +228,7 @@ private:
 
       //cout << "[AS] position: " <<  pos_info.position[l] << endl;
     }
+
     //ROS_INFO("[AS] Reading joint_states");
   }
 
@@ -203,11 +240,11 @@ private:
     err.data.resize(N_JOINT);
     //cout << "[AS] position error:" << endl;
 
-      for(short int l=0; l< N_JOINT; l++){
-        //err.data[l] = abs( arr.data[l] ) - abs( current.effort[l] ); // error = desired - current
-        err.data[l] = abs( arr.data[l] ) - abs( current.position[l] ); // position
-        //cout << "[" << l << "]: "<< abs (err.data[l]) << endl;
-      }
+    for(short int l=0; l< N_JOINT; l++){
+      //err.data[l] = abs( arr.data[l] ) - abs( current.effort[l] ); // error = desired - current
+      err.data[l] = abs( arr.data[l] ) - abs( current.position[l] ); // position
+      //cout << "[" << l << "]: "<< abs (err.data[l]) << endl;
+    }
 
     return err;
   }
@@ -221,13 +258,19 @@ private:
     bool success = true;
     ROS_INFO("[AS] executing the action call_back");
 
-    KDL::JntArray C(N_JOINT), gravity(N_JOINT);
+    KDL::JntArray C(N_JOINT), gravity(N_JOINT), tau_(N_JOINT), acc_des(N_JOINT);
     Eigen::MatrixXf qdotdot_k(N_JOINT,1), c(N_JOINT,1), g(N_JOINT,1), M_(N_JOINT, N_JOINT), tau(N_JOINT,1);
     KDL::JntSpaceInertiaMatrix M(N_JOINT);
     KDL::ChainDynParam dyn_param(mychain, KDL::Vector(0, 0, -9.80665));
     std::vector<std_msgs::Float64> msg_(N_JOINT);
     std::vector<std_msgs::Float64> msg_error(12);
     Eigen::MatrixXf position_error(N_JOINT,1);  Eigen::MatrixXf velocity_error(N_JOINT,1);
+    KDL::ChainJntToJacSolver kdl_solver(mychain);
+    KDL::Jacobian jacobian; jacobian.resize(N_JOINT);
+    Eigen::MatrixXf Jac(N_JOINT, N_JOINT); Eigen::MatrixXf CartVelCurrent(N_JOINT,1);
+    std::vector<std_msgs::Float64> msgVel(N_JOINT);
+    KDL::ChainIdSolver_RNE id_solver(mychain, KDL::Vector(0, 0, -9.80665)); // ?
+    double energy=0.0;
 
     std_msgs::Float64 a;
     std_msgs::Float64MultiArray q_des; q_des.data.resize(N_JOINT);
@@ -236,94 +279,108 @@ private:
     std_msgs::Float64 v;
     std_msgs::Float64MultiArray qddot_des ; qddot_des.data.resize(N_JOINT);
 
-    //for(short int k = 0; k < goal->trajectory.size() ; k++){
-
     std_msgs::Int64 waypoint;
     waypoint.data = goal->index.data;
 
     auto start_time = goal->trajectory[waypoint.data].time_from_start;
 
+    for (short int j = 0; j < N_JOINT; j++){
+      a.data=static_cast<float>(goal->trajectory[waypoint.data].angle_goal.data[j]);
+      q_des.data[j] = a.data;
+
+      b.data=static_cast<float>(goal->trajectory[waypoint.data].vel_goal.data[j]);
+      qdot_des.data[j] = b.data;
+
+      v.data=static_cast<float>(goal->trajectory[waypoint.data].acc_goal.data[j]);
+      qddot_des.data[j] = v.data;
+      acc_des(j) = v.data;
+    }
+
+    dyn_param.JntToMass(jointPosCurrent, M);
+    dyn_param.JntToGravity(jointPosCurrent, gravity);
+    dyn_param.JntToCoriolis(jointPosCurrent, jointVelCurrent, C);
+    kdl_solver.JntToJac(jointPosCurrent, jacobian);     // jacobian at each joint angle
+    id_solver.CartToJnt(jointPosCurrent, jointVelCurrent, acc_des, f_ext, tau_); // ?
+
+    for(short int i = 0; i < N_JOINT; i++){
+
+      c(i,0) = C(i);
+      g(i,0) = gravity(i);
+      qdotdot_k(i,0)=k_p[i]*(q_des.data[i]-q_cur(i,0))+k_d[i]*(qdot_des.data[i]-qdot_cur(i,0)) + qddot_des.data[i];
+
+      position_error(i,0) = q_cur(i,0); // TODO q_des.data[i] ;
+      velocity_error(i,0) = qdot_des.data[i];//-qdot_cur(i,0);
+
+      for(int j = 0; j < N_JOINT; j++){
+        M_(i,j) = M(i,j);
+        Jac(i,j) = jacobian(i,j);
+      }
+    }
+
+    tau = M_ * qdotdot_k + c + g;
+    CartVelCurrent = Jac * qdot_cur; // radians per seconds
+
+
+    //energy = jointEffort.data.transpose() * jointEffort.data ; // Norm
+    energy = tau_.data.transpose() * tau_.data;
+    total_E = total_E + energy ;
+
+    if(goal->finished){
+      cout << "[AS] total_E:" << total_E << endl;
+      total_E = energy = 0.0;
+    }
+
+    //cout << "Applid tau: " << tau.transpose() << ", Current tau:" << jointEffort.data.transpose() << endl;
+
+    for (short int j = 0; j < N_JOINT; j++)
+    {
+      msg_[j].data = tau(j,0);
+      pose_multiple_pub[j].publish(msg_[j]);  // publish torques to plot
+    }
+
+    for (short int j = 0; j < N_JOINT; j++)
+    {
+      msg_error[j].data = position_error(j,0);
+      msg_error[j+N_JOINT].data = velocity_error(j,0);
+      msgVel[j].data = CartVelCurrent(j,0);
+
+      data_multiple_pub[j].publish(msg_error[j]);                 // publish position error to plot
+      data_multiple_pub[j+N_JOINT].publish(msg_error[j+N_JOINT]); // publish velocity error to plot
+      data_multiple_pub[j+2*N_JOINT].publish(msgVel[j]);
+    }
+
+    do
+    {
+      std_msgs::Float64MultiArray arr ; arr.data.resize(N_JOINT);
       for (short int j = 0; j < N_JOINT; j++){
-        a.data=static_cast<float>(goal->trajectory[waypoint.data].angle_goal.data[j]);
-        q_des.data[j] = a.data;
-
-        b.data=static_cast<float>(goal->trajectory[waypoint.data].vel_goal.data[j]);
-        qdot_des.data[j] = b.data;
-
-        v.data=static_cast<float>(goal->trajectory[waypoint.data].acc_goal.data[j]);
-        qddot_des.data[j] = v.data;
+        arr.data[j] = tau(j,0);
       }
 
-      dyn_param.JntToMass(jointPosCurrent, M);
-      dyn_param.JntToGravity(jointPosCurrent, gravity);
-      dyn_param.JntToCoriolis(jointPosCurrent, jointVelCurrent, C);
+      arm_pub.publish(arr);
+      feedback.error.data.resize(N_JOINT);
+      feedback.error = calError(pos_info, q_des); // TODO: torque arr
+      action_server.publishFeedback(feedback);
 
-      for(short int i = 0; i < N_JOINT; i++){
-
-        c(i,0) = C(i);
-        g(i,0) = gravity(i);
-        qdotdot_k(i,0)=k_p[i]*(q_des.data[i]-q_cur(i,0))+k_d[i]*(qdot_des.data[i]-qdot_cur(i,0)) + qddot_des.data[i];
-
-        position_error(i,0) = q_des.data[i] ;//-q_cur(i,0);
-        velocity_error(i,0) = qdot_des.data[i];//-qdot_cur(i,0);
-
-        for(int j = 0; j < N_JOINT; j++){
-          M_(i,j) = M(i,j);}
-      }
-
-      tau = M_ * qdotdot_k + c + g;
-
-      //cout << "Applid tau: " << tau.transpose() << ", Current tau:" << jointEffort.data.transpose() << endl;
-
-      for (short int j = 0; j < N_JOINT; j++)
+      if( action_server.isPreemptRequested() || !ros::ok() ) // take care of preemption
       {
-        msg_[j].data = tau(j,0);
-        pose_multiple_pub[j].publish(msg_[j]);  // publish torques to plot
-      }
 
-      for (short int j = 0; j < N_JOINT; j++)
-      {
-        msg_error[j].data = position_error(j,0);
-        msg_error[j+N_JOINT].data = velocity_error(j,0);
-        data_multiple_pub[j].publish(msg_error[j]);                 // publish position error to plot
-        data_multiple_pub[j+N_JOINT].publish(msg_error[j+N_JOINT]); // publish velocity error to plot
-      }
-
-      do
-      {
-        std_msgs::Float64MultiArray arr ; arr.data.resize(N_JOINT);
         for (short int j = 0; j < N_JOINT; j++){
-          arr.data[j] = tau(j,0);
+          a.data=static_cast<float>(pos_info.effort[j]);   // TODO: (Position Mode) send current jointEffort as goals
+          arr.data[j] = a.data;
         }
 
-        //goal->trajectory[waypoint.data].header.stamp ;
-        // header.stamp = goal->trajectory.header + start_time ;
         arm_pub.publish(arr);
-        feedback.error.data.resize(N_JOINT);
-        feedback.error = calError(pos_info, q_des); // TODO: torque arr
-        action_server.publishFeedback(feedback);
-
-        if( action_server.isPreemptRequested() || !ros::ok() ) // take care of preemption
-        {
-
-          for (short int j = 0; j < N_JOINT; j++){
-            a.data=static_cast<float>(pos_info.effort[j]);   // send current jointEffort as goals
-            arr.data[j] = a.data;
-          }
-
-          arm_pub.publish(arr);
-          ROS_INFO("########## [AS] %s: Preempted", action_name.c_str());
-          action_server.setPreempted();
-          success = false;
-          break;
-        }
-
-        rate.sleep();
+        ROS_INFO("########## [AS] %s: Preempted", action_name.c_str());
+        action_server.setPreempted();
+        success = false;
+        break;
       }
 
-      // TODO: set the goal tolerance
-      while ( abs(feedback.error.data[0]) > 5 || abs(feedback.error.data[1]) > 5 || abs(feedback.error.data[2]) > 5 || abs(feedback.error.data[3]) > 5 || abs(feedback.error.data[4]) > 5 || abs(feedback.error.data[5]) > 5 );
-    //} //! for loop
+      rate.sleep();
+    }
+
+    // TODO: set the goal tolerance
+    while ( abs(feedback.error.data[0]) > 5 || abs(feedback.error.data[1]) > 5 || abs(feedback.error.data[2]) > 5 || abs(feedback.error.data[3]) > 5 || abs(feedback.error.data[4]) > 5 || abs(feedback.error.data[5]) > 5 );
 
     // check if succeeded--yes-->return result
     if(success)
